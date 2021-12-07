c845777f6ca4020eb6633e696e0a628344c91804
filosof777 <olimjonkamalov@mail.ru>
2021-12-07 23:30:42 +0500

    initial commit

---
+++ js/food.js
@@ -0,0 +1,32 @@
+let foods = [
+  {
+    img: "../img/food-1.jfif",
+    bgColor: "#E4F0FE",
+    title: "French Fries with Ketchup",
+    text: "$2.23"
+  },
+  {
+    img: "../img/food-2.jfif",
+    bgColor: "#FBE3F0",
+    title: "Salman and Vegatables",
+    text: "$8.99"
+  },
+  {
+    img: "../img/food-3.jfif",
+    bgColor: "#F7F8FD",
+    title: "Spaghetti with Sauce",
+    text: "$7.89"
+  },
+  {
+    img: "../img/food-4.jfif",
+    bgColor: "#E3FDF0",
+    title: "Tortelini",
+    text: "$10.26"
+  },
+  {
+    img: "../img/food-5.jfif",
+    bgColor: "#D8D7F9",
+    title: "Pizza",
+    text: "$7.23"
+  }
+];
--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-let elNavList = document.querySelector('.nav__list');
-let elHeaderBtn = document.querySelector('.header__btn');
+let elFoodList = document.querySelector('.food-list');
+let elFoodTemplate = document.querySelector('.food-temp').content;
+let elBtn = document.querySelector('.food-btn');
+let elFoodPrice = document.querySelector('.food-price'); 
+let elBtnContent = document.querySelector('.btn-content');
 
-elHeaderBtn.addEventListener('click', () => {
-  elNavList.classList.toggle('show-menu');
+for (i of foods) {
+  let elClone = elFoodTemplate.cloneNode(true);
+  elClone.querySelector('.food-inner').style.backgroundColor = i.bgColor;
+  elClone.querySelector('.food-img').src = i.img;
+  elClone.querySelector('.food-title').textContent = i.title;
+  elClone.querySelector('.food-sum').textContent = i.text;
+  elFoodList.appendChild(elClone);
+};
+
+elFoodList.addEventListener('click', (e) => {
+  if (e.target.textContent.toLowerCase() == 'add to cart') {
+    let itemPrice = e.target.parentNode.parentNode.parentNode.parentNode;
+    let itemNode = itemPrice.cloneNode(true);
+    let cancelBtn = document.createElement('button');
+    cancelBtn.className = ('btn btn-danger');
+    cancelBtn.textContent = "Cancel";
+    itemNode.appendChild(cancelBtn);
+    elFoodPrice.appendChild(itemNode);
+  }
 });
 
+elFoodPrice.addEventListener('click', (e) => {
+  if(e.target.textContent.toLowerCase() == "cancel") {
+    e.target.parentNode.style.display = 'none'
+  }
+});
